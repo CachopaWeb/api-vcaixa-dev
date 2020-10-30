a69b3ba7d92494b65afec377246aca9505c0d6e6
cachopaweb <cachopaweb@gmail.com>
2020-10-29 21:48:58 -0400

    script realocado

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const pg = require('pg')
 dotenv.config()
 
 const conexao = new pg.Client({
-   connectionString: process.env.DATABASE_URL
+   connectionString: 'postgres://oyeagktdfevyzv:84cad75cdd2217f7c32e480b0d845cec553151f824636e34d2d41abf517790d5@ec2-54-237-155-151.compute-1.amazonaws.com:5432/d25rofu8at71sa'
 });
 
 const sql_categoria = `CREATE TABLE IF NOT EXISTS CATEGORIA( 
